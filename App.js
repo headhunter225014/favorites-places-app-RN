1b164aa149d0739df903d8c473e8b63f1ace9a70
headhunter225014 <105608219+headhunter225014@users.noreply.github.com>
2023-03-30 22:01:56 -0400

    Commit nine
    Created databses using SQLLite and started t in App.js file.

--- App.js
+++ App.js
@@ -7,9 +7,27 @@ import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import {Colors} from "./constants/colors";
 import Map from "./screens/Map";
+import {useEffect, useState} from "react";
+import {init} from "./util/database";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
+    const [dbInit, setDbInit] = useState(false);
+
+    useEffect(() => {
+        init().then(() => {
+            setDbInit(true)
+        }).catch((err) => {
+            console.log(err)
+        });
+    }, []);
+
+    if (!dbInit)
+    {
+        return <AppLoading/>
+    }
   return (
     <>
       <StatusBar style='dark'/>
